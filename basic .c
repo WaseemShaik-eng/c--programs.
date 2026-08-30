648acdd81320e459c7dfa69d05ab86c993cf9890
WaseemShaik-eng <waseemshaik172@gmail.com>
2026-08-30 15:33:15 +0530

    Add files via upload

---
+++ basic .c
@@ -0,0 +1,23 @@
+#include<stdio.h> // first program
+int main(){
+    printf("Hello world:\n");
+    printf("Hello Github!\n");
+    
+int a,b;
+printf("Enter a and b values:\n"); // using operators
+scanf("%d%d",&a,&b);
+printf("addition is:%d\n",a+b);
+printf("subtraction is:%d\n",a-b);
+printf("multiplication is:%d\n",a*b);
+printf("division is:%d\n",a/b);
+printf("modulus is:%d\n",a%b);
+
+
+
+
+float area;{
+        printf("Enter  area of circle:\n"); // area of circle
+        scanf("%f",&area);
+        printf("area is:%f\n",3.14*area*area);
+return 0;
+}
