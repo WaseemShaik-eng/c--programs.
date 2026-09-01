3b4b3b026454cdf6aefefcc8bd3eb2478fb05949
Waseem.Shaik <waseemshaik172@gmail.com>
2026-09-01 15:12:52 +0530

    Implement functions to print messages

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include<stdio.h>    // print something using function
+void printHello();
+void printGit();
+int main(){
+
+ printHello();
+ printGit();
+
+ return 0;
+}
+void printHello(){
+printf("Hello world!\n");
+}
+void printGit(){
+printf("Github!\n");
+}
