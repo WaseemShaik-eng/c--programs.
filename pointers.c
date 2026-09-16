68281b277030084655041b1c8647f3af3105cc63
Waseem.Shaik <waseemshaik172@gmail.com>
2026-09-16 19:53:34 +0530

    Update pointers.c
    Added a program to demonstrate pointer usage and sum two numbers using pointers.

--- pointers.c
+++ pointers.c
@@ -1,3 +1,4 @@
+// basic pointer program
 #include<stdio.h>
 int main(){
 
@@ -6,4 +7,19 @@ int *ptr=&a;
 int _a=*ptr;
 printf("%d",_a);
     return 0;
+}
+
+
+// sum of two numbers using pointer method
+#include<stdio.h>     
+void sum(int *x, int *y);
+int main(){
+    int a=3,b=5;
+    sum(&a,&b);  // call by reference
+    return 0;
+}
+
+void sum(int *x,int *y){
+    int result=*x+*y;
+    printf("sum is:%d\n",result);
 }
